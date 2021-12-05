4db809a4be780478736c59ebf71e35c6dc90c6e1
Yilingjiang Ding <yding10@utas.edu.au>
2021-12-05 21:28:48 +0800

    add entities.

---
+++ WPF/Entities/Enums.cs
@@ -0,0 +1,31 @@
+﻿namespace RAP.Entities
+{
+    internal enum ResearcherType
+    {
+        Staff,
+        Student
+    }
+
+    internal enum Campus
+    {
+        Hobart,
+        Launceston,
+        CradleCoast
+    }
+
+    internal enum Level
+    {
+        A,
+        B,
+        C,
+        D,
+        E
+    }
+
+    internal enum PublicationType
+    {
+        Conference,
+        Journal,
+        Other
+    }
+}
---
+++ WPF/Entities/Position.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RAP.Entities
+{
+    [Table("Position")]
+    internal class Position
+    {
+        public int Id { get; set; }
+
+        public string Level { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime? End { get; set; }
+    }
+}
