6d1ff5941cc21ee8e1d2330f1c19695239d645a5
Yao Huang <yhuang36@utas.edu.au>
2021-12-06 18:12:14 +0800

    packages created

--- RAP/Database/ResearcherAdapter.cs
+++ RAP/Database/ResearcherAdapter.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RAP.Controller
+namespace RAP.Database
 {
-    class Class1
+    class ResearcherAdapter
     {
     }
 }
